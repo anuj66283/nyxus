9bddf85a223092aca230d57ca1bcbdf1db49f6aa
Anuj <anujbhattarai66283@gmail.com>
2023-11-22 13:34:04 +0545

    Delete task1 directory

--- task1/1.py
+++
@@ -1,6 +0,0 @@
-def print_even(n):
-    for i in range(1, n+1):
-        if i%2==0:
-            print(i)
-
-print_even(5)
--- task1/4.py
+++
@@ -1,9 +0,0 @@
-sentence = str(input("Enter a sentence: "))
-
-count = 0
-
-for s in sentence:
-    if s in ['a', 'e', 'i', 'o', 'u']:
-        count += 1
-
-print("Total vowel is {count}")
